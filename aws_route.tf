65768c052e2a0095fb90dc9a60ef2190757c7cb8
Gregory Mirsky <gmirsky@gmail.com>
2021-02-10 12:55:26 -0500

    formatting change

--- aws_route.tf
+++ aws_route.tf
@@ -5,7 +5,7 @@ resource "aws_route" "subnet_1_exit_route" {
 }
 
 resource "aws_route" "route_to_azure" {
-  route_table_id = aws_route_table.route_table.id
+  route_table_id         = aws_route_table.route_table.id
   destination_cidr_block = azurerm_virtual_network.vnet.address_space[0]
   gateway_id             = aws_vpn_gateway.vpn_gateway.id
 }
