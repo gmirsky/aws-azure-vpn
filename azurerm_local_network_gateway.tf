aca9faac70a400e9d178a02cbd7603b0c98dd32b
Gregory Mirsky <gmirsky@gmail.com>
2021-02-13 10:41:01 -0500

    shortened names

--- azurerm_local_network_gateway.tf
+++ azurerm_local_network_gateway.tf
@@ -1,6 +1,6 @@
 # Tunnel from Azure to AWS vpn_connection_1 (tunnel 1)
-resource "azurerm_local_network_gateway" "local_network_gateway_1_tunnel1" {
-  name                = "local_network_gateway_1_tunnel1"
+resource "azurerm_local_network_gateway" "local_network_gtwy_1_tunl_1" {
+  name                = "local_network_gtwy_1_tunl_1"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   # AWS VPN Connection public IP address
@@ -11,14 +11,14 @@ resource "azurerm_local_network_gateway" "local_network_gateway_1_tunnel1" {
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "local_network_gateway_1_tunnel1"
+      Name = "local_network_gtwy_1_tunl_1"
     }
   )
 }
 
 # Tunnel from Azure to AWS vpn_connection_1 (tunnel 2)
-resource "azurerm_local_network_gateway" "local_network_gateway_1_tunnel2" {
-  name                = "local_network_gateway_1_tunnel2"
+resource "azurerm_local_network_gateway" "local_network_gtwy_1_tunl_2" {
+  name                = "local_network_gtwy_1_tunl_2"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   gateway_address     = aws_vpn_connection.vpn_connection_1.tunnel2_address
@@ -28,14 +28,14 @@ resource "azurerm_local_network_gateway" "local_network_gateway_1_tunnel2" {
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "local_network_gateway_1_tunnel2"
+      Name = "local_network_gtwy_1_tunl_2"
     }
   )
 }
 
 # Tunnel from Azure to AWS vpn_connection_2 (tunnel 1)
-resource "azurerm_local_network_gateway" "local_network_gateway_2_tunnel1" {
-  name                = "local_network_gateway_2_tunnel1"
+resource "azurerm_local_network_gateway" "local_network_gtwy_2_tunl_1" {
+  name                = "local_network_gtwy_2_tunl_1"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   gateway_address     = aws_vpn_connection.vpn_connection_2.tunnel1_address
@@ -45,13 +45,13 @@ resource "azurerm_local_network_gateway" "local_network_gateway_2_tunnel1" {
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "local_network_gateway_2_tunnel1"
+      Name = "local_network_gtwy_2_tunl_1"
     }
   )
 }
 # Tunnel from Azure to AWS vpn_connection_2 (tunnel 2)
-resource "azurerm_local_network_gateway" "local_network_gateway_2_tunnel2" {
-  name                = "local_network_gateway_2_tunnel2"
+resource "azurerm_local_network_gateway" "local_network_gtwy_2_tunl_2" {
+  name                = "local_network_gtwy_2_tunl_2"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   gateway_address     = aws_vpn_connection.vpn_connection_2.tunnel2_address
@@ -61,7 +61,7 @@ resource "azurerm_local_network_gateway" "local_network_gateway_2_tunnel2" {
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "local_network_gateway_2_tunnel2"
+      Name = "local_network_gtwy_2_tunl_2"
     }
   )
 }
--- azurerm_virtual_network_gateway_connection.tf
+++ azurerm_virtual_network_gateway_connection.tf
@@ -1,7 +1,7 @@
-resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_connection_1_tunnel1" {
-  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gateway_1_tunnel1.id
+resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_cxn_1_tunl_1" {
+  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gtwy_1_tunl_1.id
   location                   = azurerm_resource_group.resource_group.location
-  name                       = "virtual_network_gateway_connection_1_tunnel1"
+  name                       = "virtual_network_gateway_cxn_1_tunl_1"
   resource_group_name        = azurerm_resource_group.resource_group.name
   shared_key                 = aws_vpn_connection.vpn_connection_1.tunnel1_preshared_key # AWS VPN Connection secret shared key
   type                       = "IPsec"
@@ -9,15 +9,15 @@ resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_c
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "virtual_network_gateway_connection_1_tunnel1"
+      Name = "virtual_network_gateway_cxn_1_tunl_1"
     }
   )
 }
 
-resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_connection_1_tunnel2" {
-  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gateway_1_tunnel2.id
+resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_cxn_1_tunl_2" {
+  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gtwy_1_tunl_2.id
   location                   = azurerm_resource_group.resource_group.location
-  name                       = "virtual_network_gateway_connection_1_tunnel2"
+  name                       = "virtual_network_gateway_cxn_1_tunl_2"
   resource_group_name        = azurerm_resource_group.resource_group.name
   shared_key                 = aws_vpn_connection.vpn_connection_1.tunnel2_preshared_key
   type                       = "IPsec"
@@ -25,15 +25,15 @@ resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_c
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "virtual_network_gateway_connection_1_tunnel2"
+      Name = "virtual_network_gateway_cxn_1_tunl_2"
     }
   )
 }
 
-resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_connection_2_tunnel1" {
-  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gateway_2_tunnel1.id
+resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_cxn_2_tunl_1" {
+  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gtwy_2_tunl_1.id
   location                   = azurerm_resource_group.resource_group.location
-  name                       = "virtual_network_gateway_connection_2_tunnel1"
+  name                       = "virtual_network_gateway_cxn_2_tunl_1"
   resource_group_name        = azurerm_resource_group.resource_group.name
   shared_key                 = aws_vpn_connection.vpn_connection_2.tunnel1_preshared_key
   type                       = "IPsec"
@@ -41,15 +41,15 @@ resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_c
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "virtual_network_gateway_connection_2_tunnel1"
+      Name = "virtual_network_gateway_cxn_2_tunl_1"
     }
   )
 }
 
-resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_connection_2_tunnel2" {
-  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gateway_2_tunnel2.id
+resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_cxn_2_tunl_2" {
+  local_network_gateway_id   = azurerm_local_network_gateway.local_network_gtwy_2_tunl_2.id
   location                   = azurerm_resource_group.resource_group.location
-  name                       = "virtual_network_gateway_connection_2_tunnel2"
+  name                       = "virtual_network_gateway_cxn_2_tunl_2"
   resource_group_name        = azurerm_resource_group.resource_group.name
   shared_key                 = aws_vpn_connection.vpn_connection_2.tunnel2_preshared_key
   type                       = "IPsec"
@@ -57,7 +57,7 @@ resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_c
   tags = merge(
     module.azure_user_tags.tags,
     {
-      Name = "virtual_network_gateway_connection_2_tunnel2"
+      Name = "virtual_network_gateway_cxn_2_tunl_2"
     }
   )
 }
