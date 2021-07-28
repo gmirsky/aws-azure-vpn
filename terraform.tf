38a86fd14f84ddb397137f6358becd26c32aef22
Gregory Mirsky <gmirsky@gmail.com>
2021-07-28 11:44:27 -0400

    updated provider versions

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.27.0"
+      version = "3.51.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.46.1"
+      version = "2.69.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.0.0"
+      version = "2.1.0"
     }
   }
 }
