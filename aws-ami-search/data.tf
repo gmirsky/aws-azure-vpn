b089cd77737b3c3d8406c18536b7919709fa4673
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-20 09:32:55 -0500

    formatting

--- aws-ami-search/data.tf
+++ aws-ami-search/data.tf
@@ -1,10 +1,14 @@
 data "aws_ami" "search" {
   most_recent = true
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    name = "virtualization-type"
+    values = [
+      "hvm"
+    ]
   }
   name_regex = lookup(var.amis_os_map_regex, var.os)
-  owners     = [length(var.amis_primary_owners) == 0 ? lookup(var.amis_os_map_owners, var.os) : var.amis_primary_owners]
+  owners = [
+    length(var.amis_primary_owners) == 0 ? lookup(var.amis_os_map_owners, var.os) : var.amis_primary_owners
+  ]
 }
 #
