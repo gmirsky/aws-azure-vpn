ea8c51c8ba0272080b216112ac5b8b6dd5ab3007
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-20 10:59:02 -0500

    formatting

--- azure_region/outputs.tf
+++ azure_region/outputs.tf
@@ -1,14 +1,18 @@
 output "location" {
   value       = local.regions[var.azure_region]
   description = "Converted Azure region in standard format"
+  sensitive   = false
 }
-
+#
 output "location_short" {
   value       = local.short_names[var.azure_region]
   description = "Converted Azure region in short format for resource naming purpose"
+  sensitive   = false
 }
-
+#
 output "location_cli" {
   value       = local.cli_names[var.azure_region]
   description = "Converted Azure region in Azure CLI name format"
+  sensitive   = false
 }
+#
