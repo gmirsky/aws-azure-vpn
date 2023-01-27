e6a9f1bb5743c012a14a2d37fed2e44abdc8a416
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-01-27 11:34:54 -0500

    updated terraform providder versions

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.51.0"
+      version = "4.52.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.40.0"
+      version = "3.41.0"
     }
     local = {
       source  = "hashicorp/local"
